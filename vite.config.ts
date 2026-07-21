dee681bec95a20cc10e393b1a6b943d49bb71221
super_link <po2656233@qq.com>
2026-07-21 21:45:22 +0700

    开发与正式环境配置区分

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,52 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
+// export default defineConfig(() => {
+//   return {
+//     plugins: [react(), tailwindcss()],
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, 'src'),
+//       },
+//     },
+//     server: {
+//       // HMR is disabled in AI Studio via DISABLE_HMR env var.
+//       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+//       hmr: process.env.DISABLE_HMR !== 'true',
+//       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
+//       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+//        warmup: {
+//       // 预热入口文件及高频页面组件
+//       clientFiles: [
+//         './src/main.tsx',
+//         './src/App.tsx',
+//         './src/components/LongVideoTab.tsx'
+//       ]
+//     }
+//     },
+//   };
+// });
+
+
+
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      server: {
+        warmup: {
+          clientFiles: [
+            './src/main.tsx',
+            './src/App.tsx',
+          ],
+        },
       },
+    }
+  }
+
+  return {
+    build: {
+      target: 'es2018',
+      sourcemap: false,
     },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
-      // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
-});
+  }
+})
